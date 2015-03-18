3782894397a70344f5579f8bdbe3dff4da1df4d9
RemiPrevost <prevost-r@hotmail.com>
2015-03-18 19:26:41 +0100

    commit

--- script_carte.js
+++
@@ -1,9 +0,0 @@
-function init_map() {
-	var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      google.maps.event.addDomListener(window, 'load', initialize);
-}
